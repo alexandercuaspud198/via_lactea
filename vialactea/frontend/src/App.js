d95ec86641181d8836b94671cc88e1aeb69370b4
alexander cuaspud <alexander.cuaspud198@gmail.com>
2025-10-10 13:12:12 -0500

    Actualización del sitio con nuevos cambios

--- vialactea/frontend/src/App.js
+++ vialactea/frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom"; // Cambia BrowserRouter por HashRouter
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="/via_lactea">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       <Toaster />
     </div>
   );
